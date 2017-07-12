9a9b52945435d10fc0f1c19b245d634ded8f9ea4
Nicholas Keyes <nkeyes@unm.edu>
2017-07-12 16:37:20 -0600

    missing slash?

--- Shotgun.py
+++ Shotgun.py
@@ -112,7 +112,7 @@ class Shotgun(object):
 
 
     def read_output(self, index, row):
-        filename = self.directoryName + self.filename(index, row) + ".out"
+        filename = self.directoryName + "/" + self.filename(index, row) + ".out"
         print (" The file name is :" + filename)
         myOutput = cclib.ccopen(filename)
         parsed = myOutput.parse()
