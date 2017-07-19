39a1060ed5d2a2f0df920d9ef4b3c83a70fce1e8
Nicholas Keyes <nkeyes@unm.edu>
2017-07-19 10:40:43 -0600

    again

--- Shotgun.py
+++ Shotgun.py
@@ -43,7 +43,7 @@ class Shotgun(object):
         self.results['Job Status'] =  None
         self.results['Energy'] = np.nan
         self.results['Run Time'] = np.nan
-        self.results['Freq'] = []
+        self.results['Freq'] = 0
         
         if self.size > 10:
             self.maxJobs = 10
