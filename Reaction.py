4d3de4ede913bd48c3a263ef90449d1753428c1b
Nicholas Keyes <nkeyes@unm.edu>
2017-07-21 14:43:30 -0600

    missed function change

--- Reaction.py
+++ Reaction.py
@@ -120,9 +120,9 @@ class Reaction(object):
         for key, state in self.reaction_states.items():
             for mol in state.madeUpOf:
                 data = self.parsedJSON['Molecules'][mol.name]['xyz']
-                print (self.get_output_file_name(mol))
+                print (self.get_output_file_name(mol, options))
                 sys.exit()
-                outputFile = cclib.ccopen(self.get_output_file_name(mol))
+                outputFile = cclib.ccopen(self.get_output_file_name(mol, options))
                 try:
                     parsedOutputFile = outputFile.parse()
                     newXYZ = parsedOutputFile.atomcoords[-1]
